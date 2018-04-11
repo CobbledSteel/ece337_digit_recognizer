dd2080ca3c195697c1d2598019116e31dd722b71
Chan Weng Yan <cwengyan@purdue.edu>
2018-04-11 11:06:42 -0400

    remove files; files were renamed

--- verilog/source/flex_counter.sv
+++
@@ -1,8 +0,0 @@
-// $Id: $
-// File name:   flex_counter.sv
-// Created:     4/4/2018
-// Author:      Chan Weng Yan
-// Lab Section: 337-08
-// Version:     1.0  Initial Design Entry
-// Description: Flex counter source file
-// 
--- verilog/source/synchronizer.sv
+++
@@ -1,7 +0,0 @@
-// $Id: $
-// File name:   synchronizer.sv
-// Created:     4/4/2018
-// Author:      Chan Weng Yan
-// Lab Section: 337-08
-// Version:     1.0  Initial Design Entry
-// Description: Synchronizer source file
