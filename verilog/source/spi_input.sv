cffe24b8f30ccfee98281a6dc7149268cf557542
Chan Weng Yan <cwengyan@purdue.edu>
2018-04-20 15:43:43 -0400

    initial commit

---
+++ verilog/source/spi_input.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   spi_input.sv
+// Created:     4/20/2018
+// Author:      Vadim Nikiforov
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: SPI input controller file
---
+++ verilog/source/spi_output.sv
@@ -0,0 +1,9 @@
+// $Id: $
+// File name:   spi_output.sv
+// Created:     4/20/2018
+// Author:      Chan Weng Yan
+// Lab Section: 337-08
+// Version:     1.0  Initial Design Entry
+// Description: SPI output controller file
+
+module 
