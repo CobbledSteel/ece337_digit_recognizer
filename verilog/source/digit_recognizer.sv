2147f20c8fd1c0f10b1ac0cf7d64434194c1cf27
Chan Weng Yan <cwengyan@purdue.edu>
2018-04-21 01:53:23 -0400

    initial commit

--- verilog/source/digit_recognizer.sv
+++ verilog/source/digit_recognizer.sv
@@ -7,26 +7,61 @@
 // Description: Top level file digit recognizer
 
 module digit_recognizer (
-
+	input wire clk,
+	input wire n_rst,
+	input wire SCK, SS, MOSI,
+	input wire [15:0] data,
+	output reg MISO,
+	output reg ce, oe, we,
+	output reg [15:0] address,
 );
 	// Network controller
-	// SPI input
-	// SPI output
+	
+	spi_input 	top_spi_input (		
+		.clk(clk), .n_rst(n_rst), 
+		.MOSI(MOSI), .SCK(SCK), .SS(SS),
+		.data_ready,
+		.shift_SPI,	// output
+		.SPI_in,
+		.write_en,
+		.calculate_cost,
+		.expected_label
+	);
+	
+	spi_output 	top_spi_output (
+		.clk(clk), .n_rst(n_rst), 
+		.shift_SPI, 
+		.SPI_in,
+		.SCK, SS,
+		.network_done,
+		.data_ready,
+		.cost_ready,
+		.cost_output,
+		.detected_digit,
+		.MISO(MISO)	// output
+	);
 	// pixel data register
 	// sigmoid register
 
-	// fmc
-	fmc my_fmc(
-		.clk(), 
-		.n_rst(), 
+	fmc 	top_fmc(
+		.clk(clk), .n_rst(n_rst), 
 		.ready(), 
 		.address(), 
-		.data(), 
+		.data(data), 
 		.data_out(), //output
-		.address_in(),
-		.ce(), .oe(), .we() );
+		.address_in(address),
+		.ce(ce), .oe(oe), .we(we) );
 
-	// sigmoid ALU
+	sigmoid_ALU 	top_sigmoid_ALU (
+		.clk(clk), 
+		.weight1, .weight2, .weight3, .weight4,
+		.input1, .input2, .input3, .input4,
+		.bias,
+		.accumulate,
+		.clear,
+		.out,	// output
+		.accum_out
+	);
 	// Cost calculator
 	// Detected digit
 	
