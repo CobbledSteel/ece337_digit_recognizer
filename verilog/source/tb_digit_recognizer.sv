5ca417b14540cf8c2a5b1942d38ac97224d69b1e
Chan Weng Yan <cwengyan@purdue.edu>
2018-04-21 01:55:17 -0400

    initial commit

---
+++ verilog/source/tb_digit_recognizer.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   tb_digit_recognizer.sv
+// Created:     4/21/2018
+// Author:      Chan Weng Yan
+// Lab Section: 337-08
+// Version:     1.0  Initial Design Entry
+// Description: Top level testbench for digit recognizer
