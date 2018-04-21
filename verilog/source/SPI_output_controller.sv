83e92a90adae1556bfc555fad88347049fc5840e
Vadim Nikiforov <vnikifor@purdue.edu>
2018-04-21 18:22:47 -0400

    unfinished SPI files

--- verilog/source/SPI_output_controller.sv
+++ verilog/source/SPI_output_controller.sv
@@ -6,7 +6,7 @@
 // Version:     1.0  Initial Design Entry
 // Description: SPI output controller file
 
-module spi_output (
+module SPI_output_controller (
 	input wire clk,
 	input wire n_rst,
 	input wire shift_SPI, 
@@ -20,21 +20,40 @@ module spi_output (
 	output reg MISO
 );
 	reg send_digit, send_cost; // output logic
-	reg load_en; serial_data; // ptssr
+	reg load_en, serial_data; // ptssr
 	reg [7:0] parallel_data;
-	
-	gen_pts_sr #(.NUM_BITS(4), .SHIFT_MSB(0)) spi_output_ptssr (
-		.clk(clk), 
-		.n_rst(n_rst), 
-		.shift_enable(SCK),
-		.load_enable(load_en),
-		.parallel_in(parallel_data),
-		.serial_out(serial_data)
-	); 
+
+	wire flag_long;
+	wire flag_short;
+	wire shift_output;
 
 	typedef enum logic [2:0] {idle, wait_cost, cost_sent, load_dig, dig_sent} state_type;
 	state_type state;
 	state_type next_state;
+
+
+	assign flag_short = flag_long & shift_SPI;
+	assign shift_output = shift_SPI & (state == cost_sent | state == dig_sent );
+
+	
+	flex_counter #(4) spi_input_4bit_counter(
+		.clk(clk), .n_rst(n_rst), 
+		.clear(flag_short), 
+		.count_enable(shift_SPI), 
+		.rollover_val(4'd8), 
+		.count_out(),
+		.rollover_flag(flag_long) 
+	); 
+	
+	
+	gen_pts_sr #(.NUM_BITS(8), .SHIFT_MSB(0)) spi_output_ptssr (
+		.clk(clk), 
+		.n_rst(n_rst), 
+		.shift_enable(shift_output),
+		.load_enable(load_en),
+		.parallel_in(parallel_data),
+		.serial_out(serial_data)
+	); 
 
 	always_ff @ (posedge clk, negedge n_rst)
 	begin
@@ -59,31 +78,33 @@ module spi_output (
 			wait_cost : begin
 				if (cost_ready == 1'b1)
 				next_state = cost_sent;
+				if (flag_short)
+				next_state = idle;
 				end
 			cost_sent : begin
-				if (shift_SPI == 1'b1)
+				if (flag_short == 1'b1)
 				next_state = idle;
 				end
 			load_dig : begin
 				next_state = dig_sent;
 				end
 			dig_sent : begin
-				if (shift_SPI == 1'b1)
+				if (flag_short == 1'b1)
 				next_state = idle;
 				end
 		endcase
 	end
 
 	always_comb
-	begin : 
+	begin 
 		//output logic
 		send_digit = (state == idle) | (state == load_dig);
-		send_cost = (state == wait_cost) | (state == cost_sent);
+		send_cost = (state == wait_cost) ;//| (state == cost_sent);
 		
 		// combinational logic for ptssr
 		load_en = send_digit | send_cost;
 		if (send_digit & data_ready)
-			parallel_data = detected_digit;
+			parallel_data = {4'b0000,detected_digit};
 		else if (send_cost & data_ready)
 			parallel_data = cost_output;
 		else 
@@ -93,6 +114,6 @@ module spi_output (
 		if (SS == 1'b0)
 			MISO = serial_data;
 		else 
-			MISO = z;
+			MISO = 1'bz;
 	end		
 endmodule 
