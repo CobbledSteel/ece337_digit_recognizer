5528b440c8c2fe877508d9aad42900c869ae03a5
Dustin Andree <dandree@purdue.edu>
2018-04-25 00:45:28 -0400

    Innovus update

---
+++ verilog/do_mapping.tcl
@@ -0,0 +1,48 @@
+# Step 1:  Read in the source file
+analyze -format sverilog -lib WORK { digit_recognizer_final.sv}
+elaborate digit_recognizer_final -lib WORK
+uniquify
+# Step 2: Set design constraints
+# Uncomment below to set timing, area, power, etc. constraints
+# set_max_delay <delay> -from "<input>" -to "<output>"
+
+ set_max_delay 2 -from "top_sigmoid_ALU/M1/signval" -to "top_sigmoid_ALU/M1/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M2/signval" -to "top_sigmoid_ALU/M2/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M3/signval" -to "top_sigmoid_ALU/M3/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M4/signval" -to "top_sigmoid_ALU/M4/out"
+ 
+ set_max_delay 2 -from "top_sigmoid_ALU/M1/unsignval" -to "top_sigmoid_ALU/M1/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M2/unsignval" -to "top_sigmoid_ALU/M2/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M3/unsignval" -to "top_sigmoid_ALU/M3/out"
+ set_max_delay 2 -from "top_sigmoid_ALU/M4/unsignval" -to "top_sigmoid_ALU/M4/out"
+ 
+ 
+ 
+ set_max_delay 3 -from "top_sigmoid_ALU/ADDER/in1" -to "top_sigmoid_ALU/ADDER/out"
+ set_max_delay 3 -from "top_sigmoid_ALU/ADDER/in2" -to "top_sigmoid_ALU/ADDER/out"
+ set_max_delay 3 -from "top_sigmoid_ALU/ADDER/in3" -to "top_sigmoid_ALU/ADDER/out"
+ set_max_delay 3 -from "top_sigmoid_ALU/ADDER/in4" -to "top_sigmoid_ALU/ADDER/out"
+ 
+ set_max_delay 3 -from "top_sigmoid_ALU/ACCUM/newval" -to "top_sigmoid_ALU/ACCUM/out"
+ set_max_delay 3 -from "top_sigmoid_ALU/SIGM/accum" -to "top_sigmoid_ALU/SIGM/sigma"
+
+ # set_max_delay 3 -from "top_digit_decode/count_out" -to "top_digit_decode/digit_val"
+
+# set_max_area <area>
+# set_max_total_power <power> mW
+ create_clock "clk" -name "clk" -period 5
+
+# Step 3: Compile the design
+compile -map_effort high
+
+# Step 4: Output reports
+report_timing -path full -delay max -max_paths 10 -nworst 10 > reports/digit_recognizer_final.rep
+report_area >> reports/digit_recognizer_final.rep
+report_power -hier >> reports/digit_recognizer_final.rep
+
+# Step 5: Output final VHDL and Verilog files
+write_file -format verilog -hierarchy -output "mapped/digit_recognizer_final.v"
+echo "\nScript Done\n"
+echo "\nChecking Design\n"
+check_design
+quit
---
+++ verilog/innovus.io
@@ -0,0 +1,154 @@
+# This file specifies how the pads are placed
+# The name of each pad here has to match the
+# name in the verilog code
+# The target padframe has 4 corners cells and 40 side pads
+# Each side should have at least 1 vdd/gnd pair
+# Use filler cells (PADNC) to fill up each side to 10 pads each
+# Each pad instance needs it's orientation specified
+
+Version: 2
+
+Orient: R270
+Pad: c1 NE PADFC
+
+Orient: R0
+Pad: U4 N
+Orient: R0
+Pad: U5 N
+Orient: R0
+Pad: U6 N
+Orient: R0
+Pad: U7 N
+Orient: R0
+Pad: U8 N
+Orient: R0
+Pad: P0 N PADVDD
+Orient: R0
+Pad: G0 N PADGND
+Orient: R0
+Pad: U27 N
+Orient: R0
+Pad: U28 N
+Orient: R0
+Pad: U29 N
+Orient: R0
+Pad: U30 N
+Orient: R0
+Pad: U31 N
+
+Orient: R0
+Pad: c2 NW PADFC
+
+Orient: R90
+Pad: U18 W
+Orient: R90
+Pad: U19 W
+Orient: R90
+Pad: U20 W
+Orient: R90
+Pad: U2 W
+Orient: R90
+Pad: U9 W
+Orient: R90
+Pad: U10 W
+Orient: R90
+Pad: U11 W
+Orient: R90
+Pad: U12 W
+Orient: R90
+Pad: U13 W
+Orient: R90
+Pad: U14 W
+Orient: R90
+Pad: U15 W
+Orient: R90
+Pad: U16 W
+Orient: R90
+Pad: U17 W
+Orient: R90
+Pad: U3 W
+
+Orient: R90
+Pad: c3 SW PADFC
+
+Orient: R180
+Pad: P2 S PADVDD
+Orient: R180
+Pad: G2 S PADGND
+Orient: R180
+Pad: DUM1 S PADNC
+Orient: R180
+Pad: U41 S
+Orient: R180
+Pad: U24 S
+Orient: R180
+Pad: DUM2 S PADNC
+Orient: R180
+Pad: U21 S
+Orient: R180
+Pad: DUM3 S PADNC
+Orient: R180
+Pad: U22 S
+Orient: R180
+Pad: U23 S
+Orient: R180
+Pad: DUM4 S PADNC
+Orient: R180
+Pad: U1 S
+
+Orient: R180
+Pad: c4 SE PADFC
+
+Orient: R270
+Pad: DUM5 E PADNC
+Orient: R270
+Pad: P1 E PADVDD
+Orient: R270
+Pad: G1 E PADGND
+Orient: R270
+Pad: U26 E
+Orient: R270
+Pad: U40 E
+Orient: R270
+Pad: U39 E
+Orient: R270
+Pad: U38 E
+Orient: R270
+Pad: U37 E
+Orient: R270
+Pad: U36 E
+Orient: R270
+Pad: U35 E
+Orient: R270
+Pad: U34 E
+Orient: R270
+Pad: U33 E
+Orient: R270
+Pad: U32 E
+Orient: R270
+Pad: U25 E
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
